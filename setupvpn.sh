4bc8ff573cb0dd9946928e79bce12100b0dd9b74
Kubuntu <elait@etogorod.ru>
2020-12-21 20:57:17 +0300

    HW №5. Update README.md and setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -5,6 +5,6 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A145185859
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-get --assume-yes update
 apt-get --assume-yes upgrade
-apt-get --assume-yes install iptables pritunl mongodb-org
+apt-get --assume-yes install pritunl mongodb-org
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
