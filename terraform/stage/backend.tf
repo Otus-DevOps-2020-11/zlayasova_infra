8b221ab66aab86e2e5856f38bfbb2ec2dbe91eb8
zlayasova <elait19@gmail.com>
2021-01-19 14:01:55 +0300

    HomeWork №10. Добавлены файлы для практики с ansible

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "zlayasova-otus"
     region     = "ru-central1"
     key        = "terraform/terraform.tfstate"
-    access_key = "-KQuRBoenQH34531dsffsdfaLXKv"
-    secret_key = "p4i3ysfpZwoi456t90wkljdsfjvb2EFk52oioQWEq3IkyW"
+    access_key = "qwerty123"
+    secret_key = "qwerty123"
 
     skip_region_validation      = true
     skip_credentials_validation = true
