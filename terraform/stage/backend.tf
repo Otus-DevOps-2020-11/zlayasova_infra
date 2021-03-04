f2c74c759d7896ee09039a6077fa027e6d04a813
zlayasova <elait19@gmail.com>
2021-03-04 16:52:48 +0300

    edit pipeline

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "zlayasova-otus"
     region     = "ru-central1"
     key        = "terraform/terraform.tfstate"
-    access_key = "qwerty123"
-    secret_key = "qwerty123"
+    access_key = "-KQuRBoenQHIPcI6LXKv"
+    secret_key = "p4i3ysfpZ2eFOGqJR8a3hN2EFk52oioQWEq3IkyW"
 
     skip_region_validation      = true
     skip_credentials_validation = true
